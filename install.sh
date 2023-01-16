56056a8d8e1a4dda72c8f11114ffd60ae1d11fae
MrMinemeet <alex@wtf-my-code.works>
2023-01-16 11:46:14 +0100

    Added installscript
    Currently installs version 1.27.2.13801 hardcoded

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) 2023 Alexander Voglsperger | MrMinemeet
+
+# File name for download and installation
+FILE="jetbrains-toolbox-1.27.2.13801"
+
+# Download file
+echo "Downloading $FILE.tar.gz"
+wget -O /tmp/$FILE.tar.gz https://download-cdn.jetbrains.com/toolbox/$FILE.tar.gz
+
+# Unpack file
+echo "Unpacking file..."
+tar xf /tmp/$FILE.tar.gz -C /tmp
+
+# Move file to /opt/ (recommended by JetBrains)
+echo "Installing Jetbrains Toolbox..."
+mv -f /tmp/$FILE/jetbrains-toolbox /opt/
+
+echo "Installation complete!"
